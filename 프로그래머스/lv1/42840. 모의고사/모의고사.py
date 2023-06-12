24ccbdcfee79b2adfa9d8a5c65d0f4e3ebb45387
junly21 <jaejoon0721@ajou.ac.kr>
2023-06-12 17:47:02 +0900

    [level 1] Title: 모의고사, Time: 6.80 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -0,0 +1,56 @@
+def solution(answers):
+    result = []
+    list1 = []
+    list2 = []
+    list3 = []   
+    #답안 작성
+    for i in range(len(answers)): #1번 수포자 1 2 3 4 5반복 삽입
+        if i >= 5:
+            i = i%5
+        list1.append(i+1)
+
+
+
+    list2num = [1,3,4,5] #2번 수포자 답 리스트
+    k=0
+    for i in range(len(answers)): 
+        if (i+1) % 2 == 1: # 인덱스가 홀수면 2 삽입
+            list2.append(2)
+        else: #짝수번째면 list2num 돌면서 삽입
+            if k >= 4:
+                k = k%4
+            list2.append(list2num[k]) 
+            k += 1
+       
+    list3num = [3,3,1,1,2,2,4,4,5,5]  #3번 수포자 답 list
+
+    for i in range(len(answers)+1):
+        list3.append(list3num[i%10])
+
+        
+   #1 2 3번 답안 작성 완.   
+    # 답안 체크
+    count1 = 0
+    count2 = 0
+    count3 = 0
+    
+    for i in range(len(answers)):
+        if list1[i] == answers[i]:
+            count1 += 1
+        if list2[i] == answers[i]:
+            count2 += 1
+        if list3[i] == answers[i]:
+            count3 += 1
+    
+    point = [count1, count2, count3]
+    max_score = max(point)
+    
+    if count1 == max_score:
+        result.append(1)
+    if count2 == max_score:
+        result.append(2)        
+    if count3 == max_score:
+        result.append(3)
+    print(list1, count1)
+    print(list2, count2)
+    return result
