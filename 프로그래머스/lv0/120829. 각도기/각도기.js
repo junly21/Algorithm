c01d76bc34b6573bc32a1c0ce33598ccf4581f68
junly21 <jaejoon0721@ajou.ac.kr>
2023-07-23 19:17:52 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120829. 각도기/각도기.js
@@ -0,0 +1,19 @@
+function solution(angle) {
+    var answer = 0;
+    if (0<angle && angle<90) {
+        answer = 1
+    }
+    else if(angle === 90) {
+        answer = 2
+    }
+    else if(angle > 90 && angle<180){
+        answer = 3
+    }
+    else if (angle === 180) {
+        answer = 4
+    }
+    else {
+        return 0
+    }
+    return answer;
+}
