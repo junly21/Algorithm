6354956fb381a8e8d7355339ba203351bb6cc7e0
junly21 <jaejoon0721@ajou.ac.kr>
2023-07-30 17:59:53 +0900

    [level 2] Title: 주식가격, Time: 4.80 ms, Memory: 42.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.js
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.js
@@ -1,19 +1,33 @@
+// function solution(prices) {
+//     var answer = [];
+    
+//     while(prices.length>0) {
+//         target = prices.shift()
+//         let sec = 0
+//         for(let j =0; j<prices.length; j++) {    
+//             sec += 1
+//             if(prices[j]<target){
+//                 break
+//             }
+//         }
+//         answer.push(sec)
+//     }      
+//     return answer;
+// }
+
 function solution(prices) {
     var answer = [];
     
-    for(let i = 0; i<prices.length-1; i++) {
+    for (let i =0 ; i<prices.length; i++) {
         target = prices[i]
         let sec = 0
-        
-        for(let j =i+1; j<prices.length; j++) {    
+        for(let j = i+1; j<prices.length; j++) {    
             sec += 1
             if(prices[j]<target){
                 break
             }
         }
         answer.push(sec)
-        
-    }
-    answer.push(0)        
+    }      
     return answer;
 }
