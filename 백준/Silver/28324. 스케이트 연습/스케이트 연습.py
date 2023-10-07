b440d67a9936d2d304808ec086915fdc5a105074
junly21 <jaejoon0721@ajou.ac.kr>
2023-10-07 20:16:36 +0900

    [Silver IV] Title: 스케이트 연습, Time: 388 ms, Memory: 91332 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/28324. 스케이트 연습/스케이트 연습.py
+++ 백준/Silver/28324. 스케이트 연습/스케이트 연습.py
@@ -1,26 +1,10 @@
-
 n = int(input())
+arr = list(map(int, input().split()))
+V = [0] * (n + 1)
 
-arr = list(map(int,input().split()))
-V = [n-i for i in range(n)]
-# arr.reverse()
-# V.reverse()
 
-#arr = [23 7 1 5]
-#V = [4 3 2 1]
+for i in range(n-1, -1, -1):
+    V[i] = min(arr[i], V[i + 1] + 1)
 
-#arr = [4 2 1 2 2 1]
-#  V = [6 5 4 3 2 1]
 
-#step1 n=6 i=3 n-i=3 V[5 4 3 2 2 1] 
-#setp2 n=6 i=2 
-for i in range(n-1,-1,-1):
-    if(arr[i] < V[i]):
-        V[i] = arr[i]
-        # print(V)
-
-        for j in range(i,0,-1):
-            V[j-1] = V[j]+1
-            #print(V)
-#print(V)
 print(sum(V))
