80806b16f43f1462d3be75a35f67d92d4a19d5bc
junly21 <jaejoon0721@ajou.ac.kr>
2024-02-24 15:01:55 +0900

    [Silver IV] Title: 피하자, Time: 936 ms, Memory: 153840 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/25379. 피하자/피하자.py
+++ 백준/Silver/25379. 피하자/피하자.py
@@ -9,36 +9,19 @@ evenCnt = 0
 oddCnt = 0
 oddChange = 0
 evenChange = 0
-for num in arr:
-  if num % 2 == 0:
+
+
+for i in range(0,length):
+  if arr[i] % 2 != 0:
+    oddCnt += 1
+  if arr2[i] % 2 == 0:
+     oddChange += oddCnt
+
+for i in range(0,length):
+  if arr2[i] % 2 == 0:
     evenCnt += 1
-  else: 
-    oddCnt +=1
+  if arr2[i] % 2 != 0:
+     evenChange += evenCnt
 
-def isOdd(arr):
-  for i in range(0,oddCnt):
-    if arr[i] % 2 == 0:
-      return False
-  return True
-
-while isOdd(arr) == False:
-  for i in range(0,length-1):
-    if arr[i] % 2 == 0:
-      if arr[i+1] %2 != 0:
-       arr[i] , arr[i+1] = arr[i+1], arr[i]
-       oddChange += 1
-
-def isEven(arr):
-  for i in range(0,evenCnt):
-    if arr[i] % 2 != 0:
-      return False
-  return True
-
-while isEven(arr2) == False:
-  for i in range(0,length-1):
-    if arr2[i] % 2 != 0:
-      if arr2[i+1] %2 == 0:
-       arr2[i] , arr2[i+1] = arr2[i+1], arr2[i]
-       evenChange += 1
       
 print(min(oddChange, evenChange))
